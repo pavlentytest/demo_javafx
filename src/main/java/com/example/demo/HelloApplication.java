6f5101c6536471ad777330876ce30ea45c406f90
Paul <impetus@yandex.ru>
2023-10-27 08:47:16 +0300

    simple

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -11,14 +11,21 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+       /* FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Title");
         stage.setScene(scene);
         stage.show();
+*/
+        Button btOK = new Button("OK");
+        Scene scene = new Scene(btOK, 200, 250);
+        stage.setTitle("MyJavaFX"); // Set the stage title
+        stage.setScene(scene); // Place the scene
+        stage.show();
 
 
-    }
+
+}
 
     public static void main(String[] args) {
         launch();
